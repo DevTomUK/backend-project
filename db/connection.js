db8abe644f3b00fe985e1510113a06ed50318048
DevTomUK <tomghdev@gmail.com>
2024-04-18 12:04:22 +0100

    Updated fix on connection.js

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = []
+const config = {}
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
